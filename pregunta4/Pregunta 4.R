dd2443666da8cff14c53de3197c7583b2005671f
SantosCho <105829076+SantosCho@users.noreply.github.com>
2022-05-19 09:43:21 +0800

    Add files via upload

---
+++ pregunta4/Pregunta 4.R
@@ -0,0 +1,10 @@
+
+ conjDeCartas=c("A",2:10,"J","Q","K")
+ cartas=c(rep(conjDeCartas,4))
+ tiposCarta=c("Corazon", "Diamante", "Trebol", "Espada")
+ tipos=c(rep(tiposCarta,rep(13,4)))
+ barajaCartas=data.frame(cartas,tipos)
+ barajaCartas
+ 
+ 
+ 
